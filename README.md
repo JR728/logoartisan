32a9d0bda82460b243e6314f85eb4cbfd3debfa1
JR728 <135184435+JR728@users.noreply.github.com>
2023-08-28 15:41:20 -0500

    comments and read me done

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # logoartisan
-![images](./examples/circle.svg)
-![images](./examples/circle.png)
+
+## Description
+this backend progam is to create a logo with 3 characters or more with either a circle, triangle, or square. Use node index.js and anwsers question to generate image.
+## Usage
+use npm install inquier ^8.2.6
+then use node index.js
+## Test
+use npm run test to test code from shapes.js
+## Images
+![Alt text](imgs/generated-img.png)
+![Alt text](imgs/SVG-img.png)
+## Credits
+Justin
+## License
+MIT
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+// Import required libraries
 const inquirer = require('inquirer');
 const fs = require('fs');
 const path = require('path');
 const {Triangle, Circle, Square} = require('./lib/shapes.js');
 
+// Define the questions to ask the user
 const questions = [
     {
         type: 'input',
@@ -26,17 +28,19 @@ const questions = [
         message: 'Enter shape color (keyword or hexadecimal number must include # if hexadecimal):'
     }
 ];
+// Function to write content to a file
 async function writeToFile(fileName, svgContent) {
     const examplesDir = path.join(__dirname, 'examples');
     const filePath = path.join(examplesDir, fileName);
 
-    await fs.writeFileSync(filePath, svgContent);
+    await fs.writeFileSync(filePath, svgContent);// Write the SVG content to the file
     console.log(`Generated ${fileName}`);
 
 };
+// Main initialization function
 function init() {
     inquirer
-    .prompt(questions)
+    .prompt(questions)// Prompt the user with the defined questions
     .then((answers) => {
         const shapeType = answers.shapeType;
 
@@ -45,17 +49,17 @@ function init() {
         let textY = 100;
         switch (shapeType) {
             case 'circle':
-                selectedShape = new Circle(answers.shapeColor);
+                selectedShape = new Circle(answers.shapeColor); // Create a new Circle shape
                 textX = 150;
                 textY = 100;
                 break;
             case 'triangle':
-                selectedShape = new Triangle(answers.shapeColor);
+                selectedShape = new Triangle(answers.shapeColor);// Create a new Triangle shape
                 textX = 150;
                 textY = 135;
                 break;
             case 'square':
-                selectedShape = new Square(answers.shapeColor);
+                selectedShape = new Square(answers.shapeColor);// Create a new Square shape
                 textX = 150;
                 textY = 125;
                 break;
@@ -63,17 +67,19 @@ function init() {
                 console.error('Invalid shape type.');
                 return;
         }
+        //the code for image
         const svgContent = `
         <svg version = "1.1" width="300" height="200" xmlns = "http://www.w3.org/2000/svg">
             ${selectedShape.render()}
             <text x="${textX}" y="${textY}" text-anchor="middle" alignment-baseline="middle" font-size="50px" font-weight="bold" fill="${answers.textColor || 'black'}">${answers.text}</text>
         </svg>
         `;
-        writeToFile(`logo.svg`, svgContent);
+        writeToFile(`logo.svg`, svgContent);// Generate and write the SVG content to a file
 
     })
     .catch((error) => console.error(error));
 }
 
+// Call the initialization function to start the program
 init();
 
